3f3e82d4574c86f00ec8516c4c5cd8e2eb28160e
dlwldnjsald <163090571+dlwldnjsald@users.noreply.github.com>
2024-06-13 15:11:34 +0900

    css 파일들 추가 및 users.css, UserServlet, web.xml 수정

--- src/main/java/himedia/myhome/dao/BaseDao.java
+++ src/main/java/himedia/myhome/dao/BaseDao.java
@@ -4,23 +4,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//이 클래스는 추상 클래스인 BaseDao입니다. 추상 클래스는 직접적으로 객체를 생성할 수 없으며, 
+//그 대신에 다른 클래스들이 이 클래스를 확장(상속)하여 사용할 수 있습니다.
+//데이터베이스와의 연결을 처리하는 기능을 제공하며, 
+//이 클래스를 상속받는 다른 DAO 클래스들은 이를 사용하여 데이터베이스와 상호 작용할 수 있습니다.
 public abstract class BaseDao {
 	
+	// 둘 다 null로 초기화
 	private String dbUser = null;
 	private String dbPass = null;
 	
+	//BaseDao 클래스의 생성자-두 개의 매개변수를 받아서 dbUser와 dbPass 변수를 초기화
 	public BaseDao(String dbUser, String dbPass) {
 		this.dbUser = dbUser;
 		this.dbPass = dbPass;
 	}
 	
+	//getConnection 메서드를 선언 :  데이터베이스와의 연결을 제공
 	protected Connection getConnection() throws SQLException {
+		//conn이라는 Connection 객체를 선언하고 초기화
 		Connection conn = null;
 		
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
+		try { //데이터베이스에 연결하기 위한 정보
+			Class.forName("oracle.jdbc.driver.OracleDriver"); // JDBC 드라이버를 로드
+			String dburl = "jdbc:oracle:thin:@localhost:1521:xe"; // 데이터베이스의 URL을 문자열로 저장
 			conn = DriverManager.getConnection(dburl, dbUser, dbPass);
+		
 		} catch (ClassNotFoundException e) {
 			System.err.println("JDBC Driver를 로드하지 못했습니다.");
 			e.printStackTrace();
--- src/main/java/himedia/myhome/dao/TestDao.java
+++ src/main/java/himedia/myhome/dao/TestDao.java
@@ -7,26 +7,32 @@ import himedia.myhome.vo.UserVo;
 
 public class TestDao {
 	public static void main(String[] args) {
+		
 		//	목록 출력
 		list();
 		
-		//	회원 정보 입력
+		//	회원 정보 새로 입력
 		insert("홍길동", "1234", "hong@hwalbin.org", "M");
 		insert("이영희", "4321", "younghee@lee.name", "F");
 		
-		//	목록 출력
+		//	변경된 목록 출력
 		list();
 		
-		//	회원 정보 출력
+		//	특정 회원 정보 출력
 		testGetUserByIdAndPassword("hong@hwalbin.org", "1234");
 		testGetUserByIdAndPassword("gildong@dooly.net", "whocares?");
 	}
 	
+	//특정 이메일과 비밀번호를 가진 사용자를 데이터베이스에서 조회
 	private static void testGetUserByIdAndPassword(String email, String password) {
 		
+		//UsersDao 인터페이스를 구현한 UsersDaoOracleImpl 클래스의 객체를 생성
 		UsersDao dao = new UsersDaoOracleImpl("himedia", "himedia");
+		
+		//메서드를 호출하여 해당 이메일과 비밀번호를 가진 사용자의 정보를 조회
 		UserVo vo = dao.getUserByIdAndPassword(email, password);
 		
+		//null이면 "NOT FOUND", 아니면 "FOUND"를 출력
 		System.out.println("USER " + (vo == null ? "NOT FOUND": "FOUND"));
 		System.out.println("USER INFO:" + vo);
 		
