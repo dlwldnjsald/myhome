36c78e4684fcd645ae4c4ec8432874fc70a216f8
dlwldnjsald <163090571+dlwldnjsald@users.noreply.github.com>
2024-06-14 15:15:03 +0900

    userservlet에 에러메세지 설정 추가

--- src/main/java/himedia/myhome/controller/UserServlet.java
+++ src/main/java/himedia/myhome/controller/UserServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/users")
 
@@ -31,6 +32,18 @@ public class UserServlet extends BaseServlet {
 		} else if ("joinsuccess".equals(actionName)) {
 			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/users/joinsuccess.jsp");
 			rd.forward(req, resp);
+			
+		//loginform 부분도 설정하기
+		} else if ("loginform".equals(actionName)) {
+			
+			String result = req.getParameter("result");
+			if("fail".equals(result)) {
+				//에러 메세지를 요청 어트리뷰트에 추가
+				req.setAttribute("errorMsg", "로그인에 실패했습니다");
+			}
+			
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/users/loginform.jsp");
+			rd.forward(req, resp);
 		
 		} else { //actionparameter가 없을경우
 			//홈페이지로 리다이렉트 ->
@@ -42,7 +55,7 @@ public class UserServlet extends BaseServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// a : insert -> 회원가입
-		
+		// a : login -> 로그인을 수행해주기 
 		String actionName = req.getParameter("a");
 		
 		if("join".equals(actionName)) {
@@ -63,6 +76,30 @@ public class UserServlet extends BaseServlet {
 				resp.sendRedirect(req.getContextPath() + "/users?a=joinsuccess");
 			} else {
 				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "가입 실패");
+			}
+		
+		//로그인 수행 위한 a: login -> 	
+		} else if ("login".equals(actionName))	 {
+			
+			//파라미터 얻어오기
+			String email = req.getParameter("email");
+			String password = req.getParameter("password");
+			
+			//사용자 정보 질의 
+			UsersDao dao = new UsersDaoOracleImpl(dbuser, dbpass); //dao 불러오기
+			UserVo vo = dao.getUserByIdAndPassword(email, password); //vo 받아오기
+			
+			if (vo != null) { // 사용자가 있는 경우 -> 성공
+				//사용자 정보를 세션에 기록
+					HttpSession session = req.getSession(); //세션얻어오기
+					//세션에 기록
+					session.setAttribute("authUser", vo);
+					
+					//홈화면으로 리다이렉트
+					resp.sendRedirect(req.getContextPath());
+			} else { //사용자가 없는 경우 -> 실패
+				// 다시 로그인 창으로 리다이렉트 (result=fail)
+				resp.sendRedirect(req.getContextPath() + "/users?a=loginform&result=fail");
 			}
 			
 		} else {
